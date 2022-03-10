2afdc0f8c5a0e532972b162256a5b6e4c5ebae5c
haru <haruki6180@gmail.com>
2022-03-10 17:37:53 +0900

    icon表示できた

---
+++ src/stories/atoms/LikedButton/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { AiOutlineHeart } from "react-icons/ai"
+
+export const LikedButton: React.VFC = () => {
+  return (
+    <button><AiOutlineHeart/></button>
+  );
+};
