66b78da1b3faf86e845b70a956ec1fd7ed826e5b
haru <haruki6180@gmail.com>
2022-03-11 09:52:29 +0900

    editボタンOK

---
+++ src/stories/atoms/EditButton/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+
+import { EditButton } from "."
+
+export default {
+  title: "Atoms/EditButton",
+  component: EditButton,
+} as ComponentMeta<typeof EditButton>
+
+const Template: ComponentStory<typeof EditButton> = (props) => (
+  <EditButton {...props} />
+)
+
+export const Default = Template.bind({})
---
+++ src/stories/atoms/EditButton/index.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from "react"
+import styled from 'styled-components';
+import {MdModeEdit} from 'react-icons/md'
+import COLOR from "../../../variables/color";
+
+interface Props {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+export const EditButton: React.VFC<Props> = ({onClick}) => {
+
+  return (
+    <StyledButton onClick={ onClick }>
+      <StyledShadow />
+      <MdModeEdit />
+    </StyledButton>
+  );
+};
+
+const StyledShadow = styled.div`
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+border-radius: 50%;
+background-color: ${COLOR.LIGHT_BLUE};
+transition: 0.2s;
+`
+
+const StyledButton = styled.button`
+  width: 20px;
+  height: 20px;
+  background-color: transparent;
+  border: none;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  color: ${COLOR.WHITE};
+
+  & > ${StyledShadow} {
+    opacity: 0;
+  }
+
+  &:hover {
+    & > ${StyledShadow} {
+      opacity: 0.2;
+    }
+  }
+
+`
